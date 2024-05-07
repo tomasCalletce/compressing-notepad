64fd83b46a9ad5b96c4796fed4b511e509f0bdd9
juan-c-s <juancamilo.salazar2002@gmail.com>
2024-05-07 10:16:00 -0500

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
+#include<bits/stdc++.h>
+#define el '\n'
 using namespace std;
-
 void print(const char* note){
-    cout << "Note saved: " << note << endl; 
+    cout << "Note saved: " << note << el; 
 }
 
 void compress(const char* note){
-    cout << "Compressing note..." << endl; 
+    cout << "Compressing note..." << el; 
 }
 
 void write(const string& note){
@@ -22,26 +23,34 @@ void write(const string& note){
 }
 
 
+string toLower(const string &s){
+    string resp = s;
+    for(char &c : resp)c = tolower(c);
+    return resp;
+}
 
 int main() {
     string input;
+    
     cout << "Enter command: ";
-    getline(cin, input);
+    string commands = "";
+    while((getline(cin, input)) && toLower(input)!= "quit" && input!= "exit"){
+        if(toLower(input) == "help"){
+            cout<<commands<<el;
+        }
+        else if (input == toLower("write")) {
+            cout << "Start typing and press enter to save compressed note: ";
+            string note;
+            getline(cin, note);
 
-    if (input == "write") {
-        cout << "Start typing and press enter to save compressed note: ";
-        string note;
-        getline(cin, note);
-
-        write(note);  
-    } else if (input == "edit") {
-        cout << "Enter the name of the file to edit: ";
-
-        string filename;
-        getline(cin, filename);
-    } else {
-        cout << "Unknown command" << endl;
+            write(note);  
+        } else if (input == toLower("edit")) {
+            cout << "Enter the name of the file to edit: ";
+            string filename;
+            getline(cin, filename);
+        } else {
+            cout << "Unknown command" << el;
+        }   
     }
-
     return 0;
 }
